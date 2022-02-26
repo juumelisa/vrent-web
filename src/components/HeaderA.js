70c64e5806bb58fb57461d38c98ac5e533771e4e
juumelisa <jumelisa09@gmail.com>
2022-02-26 12:20:35 +0700

    edit homepage

--- src/components/HeaderA.js
+++ src/components/HeaderA.js
@@ -7,28 +7,28 @@ export default class HeaderA extends Component{
         return(
             <>
             <div className="header-space"></div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top p-2">
-                <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top my-2">
+                <div className="container my-2">
                     <Link className="navbar-brand" to="/"><img className="img-fluid" alt="logo" width="40" height="40" src={logo} /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/vehicles">Vehicle Type</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">History</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">About</Link>
-                        </li>
-                        <li className="text-center"><Link className="btn btn-primary login" to="/login" role="button">Login</Link></li>
-                        <li className="text-center"><Link className="btn btn-primary register" to="/register" role="button">Register</Link></li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/vehicles">Vehicle Type</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">History</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">About</Link>
+                            </li>
+                            <li className="text-center"><Link className="btn btn-primary login" to="/login" role="button">Login</Link></li>
+                            <li className="text-center"><Link className="btn btn-primary register" to="/register" role="button">Register</Link></li>
                         </ul>
                     </div>
                 </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import {default as axios} from 'axios'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { FaChevronRight } from "react-icons/fa";
+import {AiFillStar} from "react-icons/ai"
 import Layout from "../components/Layout"
 import testimony from '../assets/images/edward-testimony.png'
 
 export const Homepage = () => {
     const [vehicles, setVehicles] = useState([])
     const [page, setPage] = useState({})
-
+    const navigate = useNavigate()
     useEffect(()=>{
         getVehicles()
     },[])
@@ -17,82 +19,76 @@ export const Homepage = () => {
         setVehicles(data.result)
         setPage(data.pageInfo)
     }
-        return(
-            <Layout>
-                <div className="top-wrapper">
-                    <div className="search-section">
-                        <h1>Explore and Travel</h1>
-                        <form action="/vehicles">
-                            <h3>Vehicle Finder</h3>
-                            <div className="line"></div>
-                            <input type="text" name="name" placeholder="Type the vehicle (ex. motorbike)" />
-                            <div className="selection">
-                                <select className="form-select" name="location">
-                                    <option value="">Location</option>
-                                    <option value="bali">Bali</option>
-                                    <option value="yogyakarta">Yogyakarta</option>
-                                    <option value="jakarta">Jakarta</option>
-                                    <option value="kalimantan">Kalimantan</option>
-                                    <option value="malang">Malang</option>
-                                </select>
+    const goToDetail = (id)=> {
+        navigate(`/vehicles/${id}`)
+    }
+    return(
+        <Layout>
+            <div className="search-section">
+                <div className="search-background py-5">
+                    <div className="container">
+                        <h1 className="py-5">Explore and Travel</h1>
+                        <form action="/vehicles" className="fs-5" style={{width: "100%"}}>
+                            <h2 className="fs-4 p-0 mb-5">Vehicle Finder</h2>
+                            <div className="line mb-5"></div>
+                            <input type="text" name="name" placeholder="Type the vehicle (ex. motorbike)" className="col-12"/>
+                            <div className="location-date col-12 d-flex flex-column flex-md-row p-0">
+                                <input type="text" name="location" placeholder="Location" />
                                 <div className="form-space"></div>
-                                <input className="input-date" type="date"/>
+                                <input type="date" />
                             </div>
-                            <button className="search">Search</button>
+                            <button className="search mb-5">Search</button>
                         </form>
                     </div>
                 </div>
-                <section>
-                    <div className="popular-section">
-                        <div className="popular-vehicles">
-                            <div className="sub-title d-flex flex-wrap">
-                                <h1>Popular in town</h1>
-                                <div className="other-vehicles ms-auto">
-                                    <Link to="/popular-in-town">View all <i className="fa-solid fa-angle-right"></i></Link>
-                                </div>
-                            </div>
-                            <div className="vehicles d-flex flex-wrap justify-content-center">
-                                {vehicles.map((data, idx)=>{
-                                    return(
-                                        <div className="popular-vehicle p-2 position-relative">
-                                            <Link to={data.image}>
-                                                <img src={data.image} alt="data.vehicle_name"/>
-                                                <div className="location position-absolute">
-                                                    <h6>{data.vehicle_name}</h6>
-                                                    <p>{data.location}</p>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    )
-                                })}
-                            </div>
+            </div>
+            <main className="container">
+                <div className="popular-section py-5">
+                    <div className="heading-section row">
+                        <h1 className="col-12 col-md-6 my-5">Popular in town</h1>
+                        <div className="other-vehicles col-12 col-md-6 text-end my-5">
+                            <Link to="/popular-in-town" style={{color: "#1572A1"}}>View all <FaChevronRight className="ms-3"/></Link>
                         </div>
                     </div>
-                        <div className="testimonial-section">
-                        <h2>Testimonials</h2>
-                        <div className="testimony">
-                            <div className="message d-flex flex-wrap">
-                            <div className="user-testimony p-3">
-                                <div className="star">
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
+                    <div className="row vehicles">
+                        {vehicles.map((data, idx)=>{
+                            return(
+                                <div onClick={()=>goToDetail(data.vehicle_id)} className="col-12 col-md-6 col-lg-3 popular-vehicles position-relative py-3" style={{cursor: "pointer"}}>
+                                    <img className="img-fluid" src={data.image} alt={data.vehicle_name} />
+                                    <div className="location position-absolute bottom-0 bg-white p-2">
+                                        <h6 className="m-0">{data.vehicle_name}</h6>
+                                        <p className="m-0">{data.location}</p>
+                                    </div>
                                 </div>
-                                <h5>”It was the right decision to rent vehicle here, I spent less money and enjoy the trip. It was an amazing experience to have a ride for wildlife trip!”</h5>
-                                <h6>Edward Newgate</h6>
-                                <p>Founder Circle</p>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="testimony-section py-5">
+                    <h2 className="fw-bold">Testimonials</h2>
+                    <div className="user-testimony d-flex flex-column-reverse flex-md-row justify-content-center align-items-center mx-auto p-3">
+                        <div className="message p-2">
+                            <div className="star text-center text-md-start">
+                                <AiFillStar style={{color: "ffc40c"}}/>
+                                <AiFillStar style={{color: "ffc40c"}}/>
+                                <AiFillStar style={{color: "ffc40c"}}/>
+                                <AiFillStar style={{color: "ffc40c"}}/>
+                                <AiFillStar style={{color: "ffc40c"}}/>
                             </div>
-                            <div className="user p-3">
-                                <img className="user-image" src={testimony} alt="user"/>
-                            </div>
+                            <div className="msg">
+                                <p className="text-center text-md-start fs-5 fw-normal">”It was the right decision to rent vehicle here, I spent less money and enjoy the trip. It was an amazing experience to have a ride for wildlife trip!”</p>
+                                <h6 className="text-center text-md-start fw-bold">Edward Newgate</h6>
+                                <p className="text-center text-md-start fs-6">Founder Circle</p>
                             </div>
                         </div>
+                        <div className="user">
+                            <img className="img-fluid mx-auto d-block" src={testimony} alt="user"/>
+                        </div>
                     </div>
-                </section>
-            </Layout>
-        )
+                </div>
+           </main>
+        </Layout>
+    )
 }
 
 export default Homepage
